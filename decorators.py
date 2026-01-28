3035a5212f28e6b91f503c7869ab69781becac9c
AlexTara <tarantu96@gmail.com>
2026-01-28 10:29:07 -0700

    Added new CSS + HTML + PYTHON projects built on django framework

---
+++ decorators.py
@@ -0,0 +1,14 @@
+#Takes an input and return as an output of anothr function
+
+def announce(f):
+    def wrapper():
+        print("About to run the function")
+        f()
+        print("Done with the function")
+    return wrapper
+
+@announce
+def hello():
+    print("Hello, world!")
+
+hello()
